92fe430ed4d5b08db7bad87fd032ff01b45d5ad4
anetczuk <anetczuk@o2.pl>
2018-12-31 23:33:39 +0100

    Add examples

---
+++ src/testbalancer/imu_example.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+#
+# MIT License
+#
+# Copyright (c) 2017 Arkadiusz Netczuk <dev.arnet@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Imu
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id()+"I heard %s", str(data) )
+
+
+def listener():
+
+    # in ROS, nodes are unique named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The 
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaenously.
+    rospy.init_node('imu_receiver', anonymous=True)
+
+    rospy.Subscriber("/teeterbot/imu", Imu, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+        
+if __name__ == '__main__':
+    listener()
---
+++ src/testbalancer/quat_example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import math
+import random
+import numpy as np
+import quaternion
+
+
+def v_angle(v1, v2):
+    scalar = np.dot( v1, v2 )
+    arad = math.acos( scalar )
+    adeg = math.degrees( arad )
+    print( v1, v2, "angle:", arad, adeg )
+    
+def oz_angle( v1 ):
+    oz = np.array( [0, 0, 1] )
+    v_angle( v1, oz )
+
+def qoz_angle( q1 ):
+    oz = np.array( [0, 0, 1] )
+    vecsprime = quaternion.rotate_vectors(q1, oz)
+    v_angle( vecsprime, oz )
+    
+
+if __name__ == '__main__':
+#     vecs = np.random.rand(3)
+#     vecs = np.array( [0, 0, 1] )
+    quats = quaternion.x
+#     vecs = np.array( [1, 0, 0] )
+#     vecsprime = quaternion.rotate_vectors(quats, vecs)
+#     print( quats, vecs, vecsprime )
+#     
+#     oz_angle( vecs )
+    
+    qoz_angle( quats )
