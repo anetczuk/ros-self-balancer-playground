d8f17307d50e1f38abaffbb813d2b065148b1f47
anetczuk <anetczuk@o2.pl>
2019-01-06 21:31:54 +0100

    Update plotting

---
+++ balancer-driver/plot_pitch-speed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+rqt_plot /teeterbot/right_wheel_speed /self_balancer/pitch /self_balancer/output
--- balancer-driver/plot_speed.sh
+++ balancer-driver/plot_speed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-rqt_plot /self_balancer/pitch /self_balancer/output /self_balancer/speed_pid/error
+rqt_plot /teeterbot/right_wheel_speed /self_balancer/output /self_balancer/speed_pid/error
