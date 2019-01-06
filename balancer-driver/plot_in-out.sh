39bd938c44616d285eb40dbae423d3ad3d0f54f7
anetczuk <anetczuk@o2.pl>
2019-01-07 00:38:59 +0100

    Change plot data

--- balancer-driver/plot_in-out.sh
+++ balancer-driver/plot_in-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-rqt_plot /self_balancer/pitch_pid/input /self_balancer/pitch_pid/output /self_balancer/speed_pid/input /self_balancer/speed_pid/output /self_balancer/output
+rqt_plot /self_balancer/pitch_pid/input /self_balancer/pitch_pid/output /self_balancer/speed_pid/input /self_balancer/speed_pid/output
