c37ff2bac5053f0d9db1b21afa2ccdc25dba3204
anetczuk <anetczuk@o2.pl>
2019-01-08 23:36:23 +0100

    Rename script

--- balancer-driver/plot_in-out.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-rqt_plot /self_balancer/pitch_pid/input /self_balancer/pitch_pid/output /self_balancer/speed_pid/input /self_balancer/speed_pid/output
---
+++ balancer-driver/plot_pid-cascade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+rqt_plot /self_balancer/cascade_pid/pitch/input /self_balancer/cascade_pid/pitch/output /self_balancer/cascade_pid/speed/input /self_balancer/cascade_pid/speed/output
