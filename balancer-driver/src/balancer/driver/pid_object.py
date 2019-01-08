3df930d93f25166b62aec15f5a41438b10f2a364
anetczuk <anetczuk@o2.pl>
2019-01-08 23:01:03 +0100

    Refactor: include setpoint in derivative part

--- balancer-driver/src/balancer/driver/pid_object.py
+++ balancer-driver/src/balancer/driver/pid_object.py
@@ -27,21 +27,21 @@ import rospy
 from std_msgs.msg import Float64
 
 
-class PIDObject():
+class PIDClassic():
+    '''
+    Wrapper of simple_pid.PID.
+    '''
     
-    def __init__(self, pidname, output_limit):
-        self.pid_name = pidname
+    def __init__(self, output_limit=None):
         self.output_limit = output_limit
         self.pid = None
-        self.reset_state()
         
-        rospy.Subscriber("/self_balancer/" + pidname + "/setpoint", Float64, self._setpoint_callback)
-        rospy.Subscriber("/self_balancer/" + pidname + "/kp", Float64, self._kp_callback)
-        rospy.Subscriber("/self_balancer/" + pidname + "/ki", Float64, self._ki_callback)
-        rospy.Subscriber("/self_balancer/" + pidname + "/kd", Float64, self._kd_callback)
-        self.input_pub = rospy.Publisher("/self_balancer/" + pidname + "/input", Float64, queue_size=10)
-        self.error_pub = rospy.Publisher("/self_balancer/" + pidname + "/error", Float64, queue_size=10)
-        self.output_pub = rospy.Publisher("/self_balancer/" + pidname + "/output", Float64, queue_size=10)
+        ## changes of setpoint are not included in derivative part of simple_pid.PID,
+        ## so error has to be calculaed outside of simple_pid.PID implementation
+        ## it's significant in case of PID cascade
+        self.target_point = 0.0
+        
+        self.reset_state()
     
     def is_enabled(self):
         if abs(self.pid.Kp) > 0.0001:
@@ -56,45 +56,86 @@ class PIDObject():
         self.pid.tunings = (kp, ki, kd)
     
     def set_target(self, value):
-        self.pid.setpoint = value
+        self.target_point = value
+        
+    def set_last_time(self, value):
+        self.pid._last_time = value
     
     def steer(self, inputValue):
-        if inputValue is None:
-            return None
-#         dinput = pitch - self.pid._last_input
-        self.input_pub.publish( inputValue )
-        val = self.pid( inputValue )
-#         rospy.loginfo("pid: %r -> %r di:%r es:%r p:%r", pitch, val, dinput, self.pid._error_sum, self.pid._proportional )
-        self.error_pub.publish( self.pid._error_sum + self.pid._proportional )
-        self.output_pub.publish( val )
+        error = -(self.target_point - inputValue)     ## yes, with negation sign
+        val = self.pid( error )
         return val
     
     def reset_state(self):
         ## nice params p:1.5 d:08 i:0.2 on voltage control
 #         newPid = PID(0.0, 0.0, 0.0, sample_time=None, proportional_on_measurement=False)
         newPid = PID(0.0, 0.0, 0.0, sample_time=None, proportional_on_measurement=False)
-        newPid.output_limits=(-self.output_limit, self.output_limit)
+        if self.output_limit is not None:
+            newPid.output_limits=(-self.output_limit, self.output_limit)
         newPid._last_input = 0.0
         if self.pid is not None:
             newPid.tunings = self.pid.tunings
             newPid.setpoint = self.pid.setpoint
         self.pid = newPid
     
+    def error_sum(self):
+        return self.pid._error_sum + self.pid._proportional
+    
     def state(self):
         return "%r %r" % ( self.pid._error_sum, self.pid._proportional ) 
+
+    
+class PIDObject():
+    
+    def __init__(self, pidname, output_limit=None):
+        self.pid_name = pidname
+        self.pid = PIDClassic( output_limit=output_limit )
+        
+        rospy.Subscriber("/self_balancer/" + pidname + "/setpoint", Float64, self._setpoint_callback)
+        rospy.Subscriber("/self_balancer/" + pidname + "/kp", Float64, self._kp_callback)
+        rospy.Subscriber("/self_balancer/" + pidname + "/ki", Float64, self._ki_callback)
+        rospy.Subscriber("/self_balancer/" + pidname + "/kd", Float64, self._kd_callback)
+        self.input_pub = rospy.Publisher("/self_balancer/" + pidname + "/input", Float64, queue_size=10)
+        self.error_pub = rospy.Publisher("/self_balancer/" + pidname + "/error", Float64, queue_size=10)
+        self.output_pub = rospy.Publisher("/self_balancer/" + pidname + "/output", Float64, queue_size=10)
+    
+    def is_enabled(self):
+        return self.pid.is_enabled()
+    
+    def set_params(self, kp, ki, kd):
+        self.pid.set_params(kp, ki, kd)
+    
+    def set_target(self, value):
+        self.pid.target_point( value )
+    
+    def steer(self, inputValue):
+        if inputValue is None:
+            return None
+        self.input_pub.publish( inputValue )
+        val = self.pid.steer( inputValue )
+#         rospy.loginfo("pid: %r -> %r di:%r es:%r p:%r", pitch, val, dinput, self.pid._error_sum, self.pid._proportional )
+        self.error_pub.publish( self.pid.error_sum() )
+        self.output_pub.publish( val )
+        return val
+    
+    def reset_state(self):
+        self.pid.reset_state()
+    
+    def state(self):
+        self.pid.state()
     
     def _setpoint_callback(self, value):
-        rospy.loginfo("%s setting angle: %r", self.pid_name, value )
-        self.pid.setpoint = value.data
+        rospy.loginfo("%s setting target: %r", self.pid_name, value )
+        self.pid.target_point = value.data
         
     def _kp_callback(self, value):
         rospy.loginfo("%s setting Kp: %r", self.pid_name, value )
-        self.pid.Kp = value.data
+        self.pid.pid.Kp = value.data
 
     def _ki_callback(self, value):
         rospy.loginfo("%s setting Ki: %r", self.pid_name, value )
-        self.pid.Ki = value.data
+        self.pid.pid.Ki = value.data
 
     def _kd_callback(self, value):
         rospy.loginfo("%s setting Kd: %r", self.pid_name, value )
-        self.pid.Kd = value.data
+        self.pid.pid.Kd = value.data
---
+++ balancer-driver/src/testbalancer/driver/test_pid_object.py
@@ -0,0 +1,59 @@
+# MIT License
+#
+# Copyright (c) 2017 Arkadiusz Netczuk <dev.arnet@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+
+import unittest
+
+from balancer.driver.pid_object import PIDClassic as PID
+from simple_pid.PID import _current_time
+
+
+class PIDObjectTest(unittest.TestCase):
+    def setUp(self):
+        self.pid = PID()
+        self.pid.reset_state()
+        self.pid.steer( 0.0 )             ## first call -- initialize
+
+    def tearDown(self):
+        self.pid = None
+
+    def test_setpoint_poportinal(self):
+        self.pid.set_params(1.0, 0.0, 0.0)
+        self.pid.set_target(1.0)
+        output = self.pid.steer( 2.0 )
+        self.assertEqual( -1.0, output )
+        
+    def test_setpoint_integral(self):
+        self.pid.set_params(0.0, 1.0, 0.0)
+        self.pid.set_target(1.0)
+        self.pid.set_last_time( _current_time() - 1 )      ## mimic time pass
+        output = self.pid.steer( 2.0 )
+        self.assertAlmostEqual( -1.0, output, delta=0.0001 )
+        
+    def test_setpoint_derivative(self):
+        self.pid.set_params(0.0, 0.0, 1.0)
+        self.pid.set_target(1.0)
+        output = self.pid.steer( 2.0 )
+        self.assertEqual( -1.0, output )
+        
+
