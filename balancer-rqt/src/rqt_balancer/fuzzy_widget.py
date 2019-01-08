dfd221dffbff87418bcf19a9372901369507252d
anetczuk <anetczuk@o2.pl>
2019-01-09 00:20:10 +0100

    Refactor fuzzy stub

--- balancer-rqt/src/rqt_balancer/fuzzy_widget.py
+++ balancer-rqt/src/rqt_balancer/fuzzy_widget.py
@@ -8,10 +8,10 @@ from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 
 
-class PidWidget(QWidget):
+class FuzzyWidget(QWidget):
 
-    def __init__(self, parent, pidName):
-        super(PidWidget, self).__init__(parent)
+    def __init__(self, parent):
+        super(FuzzyWidget, self).__init__(parent)
         # Create QWidget
         # Get path to UI file which should be in the "resource" folder of this package
         ui_file = os.path.join(rospkg.RosPack().get_path('rqt_balancer'), 'resource',  self.__class__.__name__ + '.ui')
@@ -19,13 +19,13 @@ class PidWidget(QWidget):
         loadUi(ui_file, self)
         # Give QObjects reasonable names
         
-        self.pid_name = pidName
-        self.widgetName.setText( pidName )
+        self.controller_name = "fuzzy"
+        self.widgetName.setText( self.controller_name )
         
-        self.setpoint_pub = rospy.Publisher('/self_balancer/' + pidName + '/setpoint', Float64, queue_size=10, latch=True)
-        self.kp_pub = rospy.Publisher('/self_balancer/' + pidName + '/kp', Float64, queue_size=10, latch=True)
-        self.ki_pub = rospy.Publisher('/self_balancer/' + pidName + '/ki', Float64, queue_size=10, latch=True)
-        self.kd_pub = rospy.Publisher('/self_balancer/' + pidName + '/kd', Float64, queue_size=10, latch=True)
+        self.setpoint_pub = rospy.Publisher('/self_balancer/' + self.controller_name + '/setpoint', Float64, queue_size=10, latch=True)
+        self.kp_pub = rospy.Publisher('/self_balancer/' + self.controller_name + '/kp', Float64, queue_size=10, latch=True)
+        self.ki_pub = rospy.Publisher('/self_balancer/' + self.controller_name + '/ki', Float64, queue_size=10, latch=True)
+        self.kd_pub = rospy.Publisher('/self_balancer/' + self.controller_name + '/kd', Float64, queue_size=10, latch=True)
          
         self.setpointSB.valueChanged.connect( self._setpointSB_changed )
         self.kpSB.valueChanged.connect( self._kpSB_changed )
@@ -33,30 +33,30 @@ class PidWidget(QWidget):
         self.kdSB.valueChanged.connect( self._kdSB_changed )
     
     def _setpointSB_changed(self, value):
-        print( self.pid_name, ' setpoint changed: ', value)
+        print( self.controller_name, ' setpoint changed: ', value)
         self.setpoint_pub.publish(value)
          
     def _kpSB_changed(self, value):
-        print( self.pid_name, ' kp changed: ', value)
+        print( self.controller_name, ' kp changed: ', value)
         self.kp_pub.publish(value)
      
     def _kiSB_changed(self, value):
-        print( self.pid_name, ' ki changed: ', value)
+        print( self.controller_name, ' ki changed: ', value)
         self.ki_pub.publish(value)
      
     def _kdSB_changed(self, value):
-        print( self.pid_name, ' kd changed: ', value)
+        print( self.controller_name, ' kd changed: ', value)
         self.kd_pub.publish(value)
     
     def save_settings(self, plugin_settings):
-        settings = plugin_settings.get_settings(self.pid_name)
+        settings = plugin_settings.get_settings(self.controller_name)
         settings.set_value("setpoint", self.setpointSB.value() )
         settings.set_value("kp", self.kpSB.value() )
         settings.set_value("ki", self.kiSB.value() )
         settings.set_value("kd", self.kdSB.value() )
 
     def restore_settings(self, plugin_settings):
-        settings = plugin_settings.get_settings(self.pid_name)
+        settings = plugin_settings.get_settings(self.controller_name)
         setpoint = settings.value("setpoint", 0)
         self.setpointSB.setValue( float(setpoint) )
         kp = settings.value("kp", 0)
