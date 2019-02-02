76736d5a56d9ccad48ac17a2edddd149c6711458
anetczuk <anetczuk@o2.pl>
2019-02-02 22:52:32 +0100

    Add reinforcement learning examples

---
+++ balancer-driver/src/testbalancer/example/openai_cartpole_example.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import gym
+from gym import spaces
+
+
+env = gym.make('CartPole-v0')
+
+print(env.action_space)
+#> Discrete(2)
+print(env.observation_space)
+#> Box(4,)
+
+print(env.observation_space.high)
+#> array([ 2.4       ,         inf,  0.20943951,         inf])
+print(env.observation_space.low)
+#> array([-2.4       ,        -inf, -0.20943951,        -inf])
+
+space = spaces.Discrete(8) # Set with 8 elements {0, 1, 2, ..., 7}
+x = space.sample()
+assert space.contains(x)
+assert space.n == 8
+
+
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+
---
+++ balancer-driver/src/testbalancer/example/pybrain_qlearn_example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from scipy import *
+import sys, time
+
+from pybrain.rl.environments.mazes import Maze, MDPMazeTask
+from pybrain.rl.learners.valuebased import ActionValueTable
+from pybrain.rl.agents import LearningAgent
+from pybrain.rl.learners import Q, SARSA
+from pybrain.rl.experiments import Experiment
+from pybrain.rl.environments import Task
+
+import pylab
+
+
+pylab.gray()
+pylab.ion()
+
+structure = array([[1, 1, 1, 1, 1, 1, 1, 1, 1],
+                   [1, 0, 0, 1, 0, 0, 0, 0, 1],
+                   [1, 0, 0, 1, 0, 0, 1, 0, 1],
+                   [1, 0, 0, 1, 0, 0, 1, 0, 1],
+                   [1, 0, 0, 1, 0, 1, 1, 0, 1],
+                   [1, 0, 0, 0, 0, 0, 1, 0, 1],
+                   [1, 1, 1, 1, 1, 1, 1, 0, 1],
+                   [1, 0, 0, 0, 0, 0, 0, 0, 1],
+                   [1, 1, 1, 1, 1, 1, 1, 1, 1]])
+
+environment = Maze(structure, (7, 7))
+
+controller = ActionValueTable(81, 4)
+controller.initialize(1.)
+
+learner = Q()
+agent = LearningAgent(controller, learner)
+
+task = MDPMazeTask(environment)
+
+experiment = Experiment(task, agent)
+
+#while True:
+for i in range(1000):
+    experiment.doInteractions(100)
+    agent.learn()
+    agent.reset()
+
+    pylab.pcolor(controller.params.reshape(81,4).max(1).reshape(9,9))
+    pylab.draw()
+
