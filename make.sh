045849e7806eafe4b9dabcf990c03f33af146380
anetczuk <anetczuk@o2.pl>
2019-08-16 23:44:25 +0200

    Update script

--- make.sh
+++ make.sh
@@ -9,4 +9,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $SCRIPT_DIR
 
 
+if [[ $* == *"--clear"* ]]; then
+    ## remove old build directory
+    build_dir=$SCRIPT_DIR/build
+    echo "Deleting: $build_dir"
+    rm -rf $build_dir
+fi
+
+
+### calling "catkin_make" is similar to following commands:
+###     mkdir $SCRIPT_DIR/build
+###     cd $SCRIPT_DIR/build
+###     cmake $SCRIPT_DIR/src
+
 catkin_make
